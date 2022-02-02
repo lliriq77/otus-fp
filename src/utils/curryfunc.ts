0e7745020f6126815e60f978cc539637f4a40a66
Kirill Tikhonov <lliriq@me.com>
2022-02-02 15:32:39 +0300

    Curryfunc

--- src/utils/curryfunc.ts
+++ src/utils/curryfunc.ts
@@ -1,28 +1,18 @@
-type argsArr = number[];
-type countSum = (...args: argsArr) => number;
+type func<T> = (...args: T[]) => T;
 
-interface Curry {
-  (f: countSum): Curried;
-}
-
-interface Curried {
-  (a: number, b: number, c: number, d: number, e: number): number;
-  (a: number, b?: number, c?: number, d?: number): Curried;
-}
-
-const sumFunc: countSum = function sum(a, b, c, d, e): number {
-  return a + b + c + d + e;
-};
-
-const curryFunc: Curry = function curryFunc(f) {
-  const curriedFunc: Curried = function curriedFunc(...args: argsArr) {
+const curryFunc = function curryFunc<T>(f: func<T>) {
+  const curriedFunc = function curriedFunc(...args: T[]): any {
     if (args.length >= f.length) {
       return f(...args);
     }
-    return (...args2: argsArr) => curriedFunc(...args.concat(args2));
+    return (...args2: T[]) => curriedFunc(...args.concat(args2));
   };
 
   return curriedFunc;
+};
+
+const sumFunc: func<number> = function sumFunc(a, b, c, d, e) {
+  return a + b + c + d + e;
 };
 
 export const hof = curryFunc(sumFunc);
